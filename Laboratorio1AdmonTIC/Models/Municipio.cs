565d7a21765584546a863c25a44ce1be55c37d95
CristianChajon0420 <cristianchajon0404@gmail.com>
2025-03-30 15:47:10 -0600

    ignorando archivos 2

---
+++ Laboratorio1AdmonTIC/Models/Municipio.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Laboratorio1AdmonTIC.Models
+{
+    public class Municipio
+    {
+        [Key]
+        public Guid MunicipioId { get; set; }
+
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        public string? Nombre {  get; set; }
+
+        [DisplayName("Codigo de Municipio")]
+        public int Codigo { get; set; }
+
+        // para hacer relación
+        [ForeignKey("DepartamentoId")]
+        public Guid DepartamentoId { get; set; }
+        public virtual Departamento Departamento { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool Inactivo { get; set; }
+    }
+}
