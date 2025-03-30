f5a896f07c501116132e01b9fa43ff77c59f4702
CristianChajon0420 <cristianchajon0404@gmail.com>
2025-03-30 15:45:32 -0600

    con git ignorado

---
+++ Laboratorio1AdmonTIC/Migrations/20250330212945_Relacion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Laboratorio1AdmonTIC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Relacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartamentoId",
+                table: "Municipios",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Municipios_DepartamentoId",
+                table: "Municipios",
+                column: "DepartamentoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Municipios_Departamentos_DepartamentoId",
+                table: "Municipios",
+                column: "DepartamentoId",
+                principalTable: "Departamentos",
+                principalColumn: "DepartamentoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Municipios_Departamentos_DepartamentoId",
+                table: "Municipios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Municipios_DepartamentoId",
+                table: "Municipios");
+
+            migrationBuilder.DropColumn(
+                name: "DepartamentoId",
+                table: "Municipios");
+        }
+    }
+}
---
+++ Laboratorio1AdmonTIC/Models/Departamento.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorio1AdmonTIC.Models
+{
+    public class Departamento
+    {
+        // agregar la pk
+        [Key]
+        public Guid DepartamentoId { get; set; }
+
+        [DisplayName ("Nombre departamento")]
+        public string? Nombre { get; set; }
+
+        [DisplayName("Codigo departamento")]
+        public int Codigo { get; set; }
+
+        [ScaffoldColumn (false)]
+        public bool inactivo { get; set; }
+    }
+}
